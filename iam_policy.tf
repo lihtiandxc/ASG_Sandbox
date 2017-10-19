25cc9c4b1c191dd7205f524d17adc7c2b3b5facb
LT <llt@hpe.com>
2017-10-19 12:20:38 +0800

    Update iam_policy.tf

--- iam_policy.tf
+++ iam_policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user_policy" "itachi001" {
-  name   = "${var.name["_user_itach001_s3_policy"]}"
+  name   = "${var.name["user_itach001_s3_policy"]}"
   user   = "itachi001"
   policy = "${data.template_file.itachi001_policy.rendered}"
 }
