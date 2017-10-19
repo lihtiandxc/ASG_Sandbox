32942d530967b518b7877e97b2d350c1567c55a9
LT <llt@hpe.com>
2017-10-19 22:02:10 +0800

    Create iam_policy.tf

---
+++ iam_policy.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_policy" "policy_itachi001" {
+  name        = "policy_itachi001"
+  path        = "/"
+  description = "My test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
