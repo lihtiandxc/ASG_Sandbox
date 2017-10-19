5e997e6f6cac218ec895bb3d2ccbe720287ab521
LT <llt@hpe.com>
2017-10-19 12:02:17 +0800

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
 #iam user policy
 data "template_file" "iam_user_itach001_policy" {
-  template = "${file("policy/iam_user_itach001_policy.json")}"
+  template = "${file("iam_user_itach001_policy.json")}"
 }
