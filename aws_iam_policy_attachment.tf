0f34dcdd57443017a77d95c011d6c0955bffa6b7
Lih Tian <lihtian@gmail.com>
2017-10-23 03:49:02 +0000

    combine policy attach and policy to iam user

--- aws_iam_policy_attachment.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_policy_attachment" "itachi001-attach" {
-  name       = "itachi001-attachment"
-#  users      = ["${aws_iam_user.user.name}"]
-   users      = ["${var.name["iam_user_itachi001"]}"]
-#  roles      = ["${aws_iam_role.role.name}"]
-#  groups     = ["${aws_iam_group.group.name}"]
-  policy_arn = "${aws_iam_policy.policy_itachi001.arn}"
-}
