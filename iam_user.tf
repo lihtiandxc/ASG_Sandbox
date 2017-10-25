e5c2cf84f09047bbee66cbf50f5a34479a06b6e4
LT <llt@hpe.com>
2017-10-25 11:42:42 +0800

    Update iam_user.tf

--- iam_user.tf
+++ iam_user.tf
@@ -7,29 +7,7 @@ resource "aws_iam_user" "itachi001" {
 resource "aws_iam_policy" "policy_itachi001" {
   name        = "policy_itachi001"
   description = "My test policy"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "s3:ListAllMyBuckets",
-            "Resource": "arn:aws:s3:::*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "s3:ListBucket",
-            "Resource": "arn:aws:s3:::fr-staging-vir-message"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": "arn:aws:s3:::fr-staging-vir-message/*"
-        }
-    ]
-}
-EOF
+  policy      = "${data.template_file.itachi001_policy.rendered}"
 }
 
 resource "aws_iam_user_policy_attachment" "test-attach" {
