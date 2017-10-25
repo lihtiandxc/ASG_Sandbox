b34e6491f25e1308e94dd89fb4f7fa2ead13bed5
Lih Tian <lihtian@gmail.com>
2017-10-25 03:54:44 +0000

    corrections syntax policy doc

--- policy_document.tf
+++ policy_document.tf
@@ -1,12 +1,10 @@
 data "aws_iam_policy_document" "itachi002" {
   statement {
-    effect = ["Allow",]
     actions = ["s3:ListAllMyBuckets",]
     resources = ["arn:aws:s3:::*",]
   }
 
   statement {
-    effect  = ["Allow",]
     actions = [
         "s3:ListBucket",
         "s3:GetBucketLocation",
@@ -18,13 +16,12 @@ data "aws_iam_policy_document" "itachi002" {
   }
 
   statement {
-    effect  = ["Allow",]
     actions = [
       "s3:*",
     ]
 
     resources = [
-      arn:aws:s3:::arn:aws:s3:::s3-putty-log-001",
+     " arn:aws:s3:::arn:aws:s3:::s3-putty-log-001",
       "arn:aws:s3:::arn:aws:s3:::s3-putty-log-001/*",
     ]
   }
