60a3231526ed68048ba00fc9c57ab995a27bda5b
Lih Tian <lihtian@gmail.com>
2017-10-25 06:41:00 +0000

    policy

--- data.tf
+++ data.tf
@@ -2,3 +2,12 @@
 data "template_file" "itachi001_policy" {
   template = "${file("itachi001_policy.json")}"
 }
+
+data "template_file" "itachi003_policy" {
+  template = "${file("itachi003_policy.json")}"
+}
+
+data "template_file" "itachi004_policy" {
+  template = "${file("itachi004_policy.json")}"
+}
+
--- iam_user.tf
+++ iam_user.tf
@@ -5,7 +5,7 @@ resource "aws_iam_user" "itachi001" {
 }
 
 resource "aws_iam_policy" "policy_itachi001v2" {
-  name        = "policy_itachi002"
+  name        = "policy_itachi001v2"
   path        = "/"
   description = "My test policy"
   policy      = "${data.template_file.itachi001_policy.rendered}"
@@ -16,6 +16,20 @@ resource "aws_iam_policy" "policy_itachi002" {
   path        = "/"
   description = "My test document policy"
   policy      = "${data.aws_iam_policy_document.itachi002.json}"
+}
+
+resource "aws_iam_policy" "policy_itachi003" {
+  name        = "policy_itachi003_console"
+  path        = "/"
+  description = "My test document policy"
+  policy      = "${data.template_file.itachi003_policy.rendered}"
+}
+
+resource "aws_iam_policy" "policy_itachi004" {
+  name        = "policy_itachi004_console"
+  path        = "/"
+  description = "My test document policy"
+  policy      = "${data.template_file.itachi004_policy.rendered}"
 }
 
 #resource "aws_iam_user_policy_attachment" "test-attach" {
