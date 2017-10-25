35f60f1b68cd039b285d7d1047ffc5781ebab263
LT <llt@hpe.com>
2017-10-25 11:45:01 +0800

    Update iam_user.tf

--- iam_user.tf
+++ iam_user.tf
@@ -6,8 +6,16 @@ resource "aws_iam_user" "itachi001" {
 
 resource "aws_iam_policy" "policy_itachi001" {
   name        = "policy_itachi001"
+  path        = "/"
   description = "My test policy"
   policy      = "${data.template_file.itachi001_policy.rendered}"
+}
+
+resource "aws_iam_policy" "policy_itachi002" {
+  name        = "policy_itachi002"
+  path        = "/"
+  description = "My test document policy"
+  policy      = "${data.aws_iam_policy_document.itachi002.json}"
 }
 
 resource "aws_iam_user_policy_attachment" "test-attach" {
